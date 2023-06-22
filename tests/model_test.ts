f621171b87c307ff781fa3187beca52845fa2146
Yoshihiro Fujimoto <fuji.44mt@gmail.com>
2023-06-22 12:12:07 +0900

    fix: first line was missing and has been corrected

--- tests/model_test.ts
+++ tests/model_test.ts
@@ -1,8 +1,14 @@
 import { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
 import { SFCardCsvLoader } from "../model.ts";
 
-Deno.test(async function addTest() {
-  const loader = new SFCardCsvLoader("./tests/model_test.csv");
+Deno.test(async function basicHistory() {
+  const loader = new SFCardCsvLoader("./tests/basic.csv");
+  await loader.load();
+  assertEquals(loader.records.length, 20);
+});
+
+Deno.test(async function commuterPassHistory() {
+  const loader = new SFCardCsvLoader("./tests/commuter_pass.csv");
   await loader.load();
   assertEquals(loader.records.length, 20);
 });
