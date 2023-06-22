224e7545f3722a2a544215a431a58f41c689a615
Yoshihiro Fujimoto <fuji.44mt@gmail.com>
2023-06-22 10:36:17 +0900

    feat: impl json sub command

--- main.ts
+++ main.ts
@@ -1,33 +1,24 @@
-import {
-  Command,
-  EnumType,
-} from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
+import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
 import { SFCardCsvLoader, SFCardRecord } from "./model.ts";
-
-const ConvertType = new EnumType(["json"]);
 
 function printJSON(records: SFCardRecord[]) {
   console.log(JSON.stringify(records));
+}
+
+function createJsonCommand() {
+  return new Command()
+    .description("JSONに変換します")
+    .arguments("<csv-path:string>")
+    .action(async (_, csvPath) => {
+      const loader = new SFCardCsvLoader(csvPath);
+      await loader.load();
+      printJSON(loader.records);
+    });
 }
 
 await new Command()
   .name("sfcv")
   .description("SFCard Viewerで出力したCSVを変換します")
   .version("v0.1.0")
-  .type("convert-type", ConvertType)
-  .option("-t, --type <type:convert-type>", "変換する形式", {
-    default: "json",
-  })
-  .arguments("<csv-path:string>")
-  .action(
-    async ({ type }, csvPath) => {
-      const loader = new SFCardCsvLoader(csvPath);
-      await loader.load();
-      switch (type) {
-        case "json":
-          printJSON(loader.records);
-          break;
-      }
-    },
-  )
+  .command("json", createJsonCommand())
   .parse(Deno.args);
